7f2e8111123a1b1020a076b45409bf081545f07c
sarashin <sarashina.nikki65@gmail.com>
2020-04-19 12:05:33 +0900

    fix delay delete

--- content_scripts.js
+++ content_scripts.js
@@ -2,23 +2,30 @@ var url = location.href;
 
 function delete_rate() {
     var rating_display = document.getElementsByClassName('glicko_rating_area__box');
+    if (rating_display[0] === undefined) return false;
     rating_display[0].remove();
+    console.log('deleted!');
+    return true;
 }
 
 function delay_delete_rate() {
-    const jsInitCheckTimer = setInterval(jsLoaded, 500);
-    function jsLoaded() {
-        if (document.getElementsByClassName('glicko_rating_area__box') != null) {
-            clearInterval(jsInitCheckTimer);
-            delete_rate();
+    const js_timer = setInterval(is_deleted, 500);
+    function is_deleted() {
+        if (delete_rate() === true) {
+            clearInterval(js_timer);
+        }
+        else {
+            console.log('not yet deleted');
         }
     }
 }
 
-if (url.indexOf('result') !== -1) {
-    if (document.getElementsByClassName('glicko_rating_area__box') !== null) delete_rate();
+console.log(url);
+if (url.indexOf('result') === -1) {
+    console.log('not result');
+    delete_rate();
 }
 else {
-    window.addEventListener('load', delete_rate(), false);
+    console.log('result');
     delay_delete_rate();
 }
