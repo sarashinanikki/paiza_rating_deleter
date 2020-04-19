4c281d0e2a6cd9fe7fb53620a108ba87dd53a11e
sarashin <sarashina.nikki65@gmail.com>
2020-04-19 11:09:33 +0900

    adapt to delay load

--- content_scripts.js
+++ content_scripts.js
@@ -1,2 +1,17 @@
-var rating_display = document.getElementsByClassName('glicko_rating_area__box');
-rating_display[0].remove();
+var url = location.href;
+
+function delete_rate() {
+    var rating_display = document.getElementsByClassName('glicko_rating_area__box');
+    rating_display[0].remove();
+}
+
+function delay_delete_rate() {
+    window.addEventListener('load',delete_rate(), false);
+}
+
+if (url.indexOf('result') !== -1) {
+    delete_rate();
+}
+else {
+    delay_delete_rate();
+}
